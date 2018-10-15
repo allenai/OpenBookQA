4413f931d630725f411c97decec188c66b76202f
Dirk Groeneveld <dirkg@allenai.org>
2018-10-15 11:54:53 -0700

    Safety checks for experiments

--- train_everything.sh
+++ train_everything.sh
@@ -3,6 +3,11 @@
 set -x
 set -e
 
+EXPERIMENT_NAME=$1
+
+test -e _experiments && (echo "_experiments exists, bailing" && exit 1)
+test -e $EXPERIMENT_NAME && (echo "$EXPERIMENT_NAME exists, bailing" && exit 1)
+
 parallel --line-buffer -j3 'bash scripts/experiments/qa/run_experiment_openbookqa.sh' ::: \
   training_config/qa/multi_choice/openbookqa/reader_mc_qa_question_to_choice.json \
   training_config/qa/multi_choice/openbookqa/reader_mc_qa_question_to_choice_elmo.json \
@@ -14,3 +19,5 @@ parallel --line-buffer -j3 'bash scripts/experiments/qa/run_experiment_openbookq
   training_config/qa/multi_choice/openbookqa/knowreader_v1_mc_qa_multi_source_cn5wordnet.json \
   training_config/qa/multi_choice/openbookqa/knowreader_v1_mc_qa_multi_source_openbook_plus_cn5omcs.json \
   training_config/qa/multi_choice/openbookqa/knowreader_v1_mc_qa_multi_source_openbook_plus_cn5wordnet.json
+
+mv _experiments $EXPERIMENT_NAME
