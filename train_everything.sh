f72c8f005822f6b6e046e760ab7223e9f806d1b6
Dirk Groeneveld <dirkg@allenai.org>
2018-10-15 14:57:38 -0700

    Run on GPUs

--- train_everything.sh
+++ train_everything.sh
@@ -8,7 +8,7 @@ EXPERIMENT_NAME=$1
 test -e _experiments && (echo "_experiments exists, bailing" && exit 1)
 test -e $EXPERIMENT_NAME && (echo "$EXPERIMENT_NAME exists, bailing" && exit 1)
 
-parallel --halt now,fail=1 --line-buffer -j3 'bash scripts/experiments/qa/run_experiment_openbookqa.sh' ::: \
+parallel --halt now,fail=1 --line-buffer -j3 'bash scripts/experiments/qa/run_experiment_openbookqa_gpu.sh' ::: \
   training_config/qa/multi_choice/openbookqa/reader_mc_qa_question_to_choice.json \
   training_config/qa/multi_choice/openbookqa/reader_mc_qa_question_to_choice_elmo.json \
   training_config/qa/multi_choice/openbookqa/reader_mc_qa_esim.json \
