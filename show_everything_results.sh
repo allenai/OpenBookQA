8636fc940b9e9264713b96d0be6f568033429e68
Dirk Groeneveld <dirkg@allenai.org>
2018-10-18 11:51:47 -0700

    Look at training output, not validation

--- show_everything_results.sh
+++ show_everything_results.sh
@@ -5,9 +5,9 @@ EXPERIMENT=$1
 set -e
 
 ls -1 $EXPERIMENT/*/metrics.json | \
-parallel 'echo {} $(jq .validation_accuracy_avg {})' | \
+parallel 'echo {} $(jq .test_accuracy_avg {})' | \
 sort | \
-sed -Ee 's/^[^/]*\/+//' | \
 sed -e "s/\/metrics.json//" | \
 sed -Ee 's/\.json_([0-9][0-9]-){6}r[0-9]+//' | \
+sed -e "s/^[^ ]*\/\+//" | \
 csvtool -t " " -u TAB transpose -
